8348aea5eae5e52f38c5567b1de8f6f069c9cca7
Abhinav Jha <linuxlenin04@gmail.com>
2023-01-01 12:31:52 +0000

    helper methods for the Lexer

--- Scanner.cs
+++ Scanner.cs
@@ -42,11 +42,53 @@ public class Scanner
             case '+': AddToken(TokenType.PLUS); break;
             case ';': AddToken(TokenType.SEMICOLON); break;
             case '*': AddToken(TokenType.STAR); break;
+            case '!':
+                AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+            case '/':
+                if (Match('/'))
+                {
+                    // a comment goes on until the end of the line
+                    while (Peek() != '\n' && !IsAtEnd()) Advance();
+                }
+                else
+                {
+                    AddToken(TokenType.SLASH);
+                }
+                break;
 
             default:
                 Baroque.Error(line, String.Format("Unexpected character {0}", c));
                 break;
         }
+    }
+
+    // checks if the character at the current position is equal to the character passed in
+    // it's like a conditional Advance, it only consumes the character if it matches what we want
+    private bool Match(char expected)
+    {
+        if (IsAtEnd()) return false;
+        if (source[current] != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    // it's like Advance(), but doesn't consume the character
+    private char Peek()
+    {
+        if (IsAtEnd()) return '\0';
+
+        return source[current];
     }
 
     private bool IsAtEnd()
